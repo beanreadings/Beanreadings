412638759b9bee7ef9db52e07123fc16a2b6255c
LimitFinity <54669985+TuffTomy@users.noreply.github.com>
2024-03-20 18:14:18 +1100

    build (batch edition)

--- build.bat
+++ build.bat
@@ -1,2 +1,11 @@
 @Echo off
-echo I don't know batch lmao, used WSL, Arch (i use arch btw) and Macos my whole life :rofl:
+del /s /q dist
+mkdir dist
+cargo build --release
+move target\wasm32-unknown-unknown\release\simulation.wasm dist\simulation.wasm
+wasm-bindgen dist\simulation.wasm --out-dir dist --target web
+copy index.html dist\index.html
+copy index.js dist\index.js
+copy dist\simulation.js simulation.js
+cd dist
+ws
