2871d7be6ee81c651931e40bd9d3af1ddf53a571
Aito Stukas <aito@outlook.com.au>
2024-05-02 06:56:48 +1000

    starting to work on stuff

--- README.md
+++ README.md
@@ -13,5 +13,18 @@ This builds the rust code into WebAssembly and then builds the website using the
 ## Forecasts
 Beanreadings has predicted the following:
 
+## Bibliography
+[NIH Calcium Information](https://ods.od.nih.gov/factsheets/Calcium-HealthProfessional/)
+[ARS FPED Data Tables 2017-2020](https://www.ars.usda.gov/ARSUserFiles/80400530/pdf/fped/Table_1_FPED_GEN_1720.pdf)
+[NIH Fibre Intake for Optimal Health](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9298262/)
+[NIH Smoking](https://www.ncbi.nlm.nih.gov/books/NBK537066/)
+[NIH Alcohol effects in the United States](https://www.niaaa.nih.gov/alcohols-effects-health/alcohol-topics/alcohol-facts-and-statistics/alcohol-use-united-states-age-groups-and-demographic-characteristics#prevalence-of-past-month-drinking)
+[NIH Scope of Nicotine Use](https://nida.nih.gov/publications/research-reports/tobacco-nicotine-e-cigarettes/what-scope-tobacco-use-its-cost-to-society)
+[NIH Level of Physical Activity](https://pubmed.ncbi.nlm.nih.gov/31438909/)
+[CDC Sleep](https://www.cdc.gov/sleep/data-and-statistics/adults.html)
+[NIH Anti Vaccination](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9009899/)
+
+As we believe the National Institute of Health is a reliable source, we have used their information to make our predictions. We have also used the USDA's data tables, and CDC's statistics. Every site on this list should be from a reliable source, preferably from a government page. As the government of the United States is a large government, with much data and abundant research papers, much of our research and simulations are based on data from their sources.
+
 ## Notes (we will delete this later)
 The death rate is increasing around 1.5% per year.
--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,95 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub struct Diet {
     // 1.00 = 100% of what we need, 0.50 = 50% of what we need
+
+    // calcium: 1000mg required per day, average is around 1000mg which is good
     calcium: f64,
+
+    // sugar: 50g max per day with a 2000 calorie diet, average is around 100g which is bad
+    sugar: f64,
+
+    // solid fats: includes trans fats, saturated fats. 24g max per day with a 2000 calorie diet
+    // average intake is 37g which is bad. increases risk of cardiovascular disease
+    solid_fats: f64,
+
+    // fibre: who recommends 25g per day, average intake is 15g which is bad
+    fibre: f64,
+
+    // salt: generally less than 2.3g per day, average intake is 3.4g which is bad
+    salt: f64,
 }
 
 impl Default for Diet {
     fn default() -> Self {
-        Self { calcium: 1.0 }
+        // for diet, we assign them each a weight and what they corrolate to in the deaths category.
+        // The weight will probably be calculated by something similar to Stochastic Gradient Descent
+        // to optimize the weights and their corrolation on the death rate.
+        Self {
+            calcium: 1.0,    // we get 100% of the calcium we need, which is good
+            sugar: 2.0,      // we get 2x the sugar we can get, maximum
+            solid_fats: 1.5, // we get 1.5x the solid fats we can get, maximum
+            fibre: 0.6,      // we get 60% of the fibre we need, which is bad
+            salt: 1.5,       // we get 1.5x the salt we can get, maximum
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct Habits {
+    // similar to diet, we assign them each a weight and what they corrolate to in the deaths category.
+    // The weight will probably be calculated by something similar to Stochastic Gradient Descent
+    // to optimize the weights and their corrolation on the death rate.
+    // 1.00 = 100% of what we need, 0.50 = 50% of what we need
+
+    // smoking: 0.5 = 50% of the population smokes, 1.0 = 100% of the population smokes
+    smoking: f64,
+
+    // alcohol: 0.5 = 50% of the population drinks, 1.0 = 100% of the population drinks (per month)
+    alcohol: f64,
+
+    // binge drinking: 0.5 = 50% of the population binge drinks, 1.0 = 100% of the population binge drinks (per month)
+    binge_drinking: f64,
+
+    // vaping: 0.5 = 50% of the population vapes, 1.0 = 100% of the population vapes
+    vaping: f64,
+
+    // proper exercise: 0.5 = 50% of the population exercises, 1.0 = 100% of the population exercises
+    exercise: f64,
+
+    // proper sleep: 0.5 = 50% of the population gets proper sleep, 1.0 = 100% of the population gets proper sleep
+    sleep: f64,
+
+    // proper hydration: 0.5 = 50% of the population is properly hydrated, 1.0 = 100% of the population is properly hydrated
+    hydration: f64,
+
+    // anti-vaccination: 0.5 = 50% of the population is anti-vaccination, 1.0 = 100% of the population is anti-vaccination
+    anti_vaccination: f64,
+
+    // drugs: 0.5 = 50% of the population uses drugs, 1.0 = 100% of the population uses drugs
+    drugs: f64,
+}
+
+impl Default for Habits {
+    fn default() -> Self {
+        Self {
+            smoking: 0.23, // 23% of the population smokes
+
+            alcohol: 0.52, // its done based on a monthly-basis, 52% of the population drinks every month
+
+            binge_drinking: 0.23, // its done based on a monthly-basis, 23.5% of the population binge drinks every month
+
+            vaping: 0.05, // 5% of the population vapes, this is in 2022, above 12 years old
+
+            exercise: 0.55, // 55% of the population exercises properly
+
+            sleep: 0.63, // 63% of the population gets proper sleep
+
+            hydration: 0.25, // 25% of the population is properly hydrated. Average is 2.5 cups per day
+
+            anti_vaccination: 0.05, // 5% of the population is anti-vaccination,
+
+            drugs: 0.05, // 5% of the population uses drugs
+        }
     }
 }
 
@@ -64,10 +147,17 @@ impl Simulation {
 
         // now we calculate death rates for each scenario
 
-        let cardiovacular_disease_rate = 0.1;
-
         serde_json::to_string_pretty(&serde_json::json!({
-            "loaded": true
+            "settings": {
+                "diet": {
+                    "calcium": config.diet_settings.calcium,
+                    "sugar": config.diet_settings.sugar,
+                    "solid_fats": config.diet_settings.solid_fats,
+                    "fibre": config.diet_settings.fibre,
+                    "salt": config.diet_settings.salt,
+                },
+            },
+
         }))
         .unwrap()
     }
