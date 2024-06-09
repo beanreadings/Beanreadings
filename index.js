1a8dad1374e8272d663981c529b1f5f448631871
Aito Murai (Aityz) <aityz@192-168-1-124.tpgi.com.au>
2024-06-09 10:32:38 +1000

    JS Template

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-import init, {
-  Simulation,
-  SimulationConfig,
-  Habits,
-  Diet,
-} from "./simulation.js";
+import init, { Simulation, Settings, SimulationResult } from "./simulation.js";
+
 await init();
 export function simulate() {
   // we do stuff
 
   console.log("Attempting to simulate");
 
-  let food = new Diet(1.0, 2.0, 1.5, 0.6, 1.5);
+  let settings = new Settings();
 
-  console.log("Food: ", food);
+  settings.set_population(1000);
 
-  let habits = new Habits(0.23, 0.52, 0.23, 0.05, 0.55, 0.63, 0.25, 0.05, 0.13);
+  let simulation = new Simulation(settings);
 
-  console.log("Habits: ", habits);
+  let simil = simulation.long();
 
-  let simulation_config = new SimulationConfig(
-    food,
-    habits,
-    BigInt(10),
-    BigInt(1000),
-  );
+  console.log(simil.population);
 
-  console.log("Simulation Config: ", simulation_config);
-
-  let simulation = new Simulation(simulation_config);
-
-  console.log("Simulation: ", simulation);
-
-  let result = simulation.simulate();
-
-  document.getElementById("result").innerHTML = result;
+  console.log(simil.get_population_curve());
 }
